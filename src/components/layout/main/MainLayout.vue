f52906028187e56258a2804ac8ee02f04177f929
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2024-01-21 01:48:00 +0700

    feat: Main layout.

---
+++ src/components/layout/main/MainLayout.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import FooterBar from '../../ui/footer/FooterBar.vue'
+import NavBar from '../../ui/navbar/NavBar.vue'
+import { useNavBar } from '../../ui/navbar/navbar'
+import { cn } from '@/lib/utils'
+
+const { isNavBarExpanded, toggleNavBarExpanded } = useNavBar()
+</script>
+
+<template>
+  <div
+    :class="cn('flex min-h-screen flex-col bg-background', isNavBarExpanded && 'overflow-hidden')"
+  >
+    <!-- NavBar (Header) -->
+    <NavBar
+      :is-nav-bar-expanded="isNavBarExpanded"
+      @toggle-nav-bar-expanded="toggleNavBarExpanded"
+    />
+
+    <!-- Content -->
+    <slot></slot>
+
+    <!-- Footer -->
+    <FooterBar />
+  </div>
+</template>
