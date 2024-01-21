06323095d5e69c1ca8756d7eef824a9b1d8e9922
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2024-01-21 09:37:21 +0700

    feat: Home page.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,42 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ScnButton } from '@/components/ui/button'
+import { ArrowRightCircle } from 'lucide-vue-next'
+import type { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main className="flex flex-auto flex-col">
+    <!-- Hero Section -->
+    <section
+      className="flex min-h-[calc(100vh-90px)] w-full items-center justify-center px-6 py-16 sm:p-12 lg:p-24"
+    >
+      <div className="flex max-w-7xl flex-col-reverse items-center gap-6 sm:flex-row lg:gap-16">
+        <!-- Hero Texts -->
+        <div class="flex flex-col items-start gap-3 lg:gap-6">
+          <h1 data-cy="hero-title" class="text-4xl font-bold lg:text-6xl">Shorten URL Easily.</h1>
+          <p
+            data-cy="hero-description"
+            class="text-base font-normal text-muted-foreground lg:text-xl"
+          >
+            With Snip URL we help you shortern your URL easily and quickly so it is easier to share
+            and remember.
+          </p>
+          <RouterLink to="/snip" data-cy="hero-link">
+            <ScnButton size="lg">
+              Try Now
+              <ArrowRightCircle class="ml-2 h-5 w-5" />
+            </ScnButton>
+          </RouterLink>
+        </div>
+
+        <!-- Hero Image -->
+        <img
+          src="/logo-cropped.png"
+          alt="Snip URL Logo"
+          data-cy="hero-image"
+          className="w-[250px] lg:w-[420px]"
+        />
+      </div>
+    </section>
   </main>
 </template>
